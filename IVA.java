7fcb94cbc2f01d93af672af3191b60d0c1f6127b
Skata00C <pablocoresalvarez18@gmail.com>
2026-09-23 13:24:44 +0200

    Ayado ejercicio IVA

--- IVA.java
+++ IVA.java
@@ -6,9 +6,13 @@ import java.util.Scanner;
 
 public class IVA {
         public static void main(String[] args) {
-        final double IVA = 1.21;
 
-        Scanner sc = new Scanner(System.in);                // Abro el Scanner para preguntarle al usuario el precio del producto al cual queramos aplicarle el IVA.
+        Scanner sc = new Scanner(System.in);    // Abro el Scanner para preguntarle al usuario el porcentaje de IVA al cual queramos aplicarle al producto.
+
+        System.out.print("Escribe el porcentaje del IVA:");
+        double iva = sc.nextDouble();
+        System.out.println("El porcentaje de iva es:" + iva + "%");
+        System.out.println("============");
 
         System.out.print("Escribe el valor del producto:");
         double valorNetoDelProducto = sc.nextDouble();      // Gracias al sc.nextDouble el programa preguntará al Usuario cual es el valor neto del producto.
@@ -16,7 +20,11 @@ public class IVA {
 
         sc.close();                                         // Cerramos el sc. para permitir que el programa deje de consumir recursos, ya que no necesitaremos ninguna respuesta más por el Usuario.
 
-        System.out.println("El precio del producto aplicado con el IVA es:" + valorNetoDelProducto * IVA);  // Realizamos la operación para obtener el resultado querido.
+        double valorIvaProducto = (valorNetoDelProducto * (iva / 100));  // Realizamos la operación para obtener el resultado querido.
+        System.out.println("Valor del IVA requerido del producto:" + valorIvaProducto);
+        System.out.println("============");
+        System.out.println("Valor final del producto con el IVA requerido aplicado:" + (valorNetoDelProducto + valorIvaProducto)); // Operación final para optener el valor del producto con el IVA requerido aplicado.
+
 
     }
 
--- Triangulo.java
+++ Triangulo.java
@@ -9,17 +9,17 @@ public class Triangulo {
 
         Scanner sc = new Scanner(System.in); // Realizo el Scanner para poder darle valores a las varibles de la equiación
         
-        System.out.print("Escribe la variable b:");
+        System.out.print("Escribe la variable b (cm):");
         double b = sc.nextDouble();
         System.out.println("=============="); // Espacio por cuestiones estéticas
 
-        System.out.print("Escribe la variable :");
+        System.out.print("Escribe la variable h (cm):");
         double h = sc.nextDouble();
         System.out.println("=============="); // Espacio por cuestiones estéticas
         
         sc.close(); // Cerramos el Scanner por cuestiones de rendimiento
 
-        System.out.print("El área del triangulo es:)" + (b  * h) / 2.) ;
+        System.out.print("El área del triangulo es:)" + (b  * h) / 2. + " cm. ");
 
     }
 
